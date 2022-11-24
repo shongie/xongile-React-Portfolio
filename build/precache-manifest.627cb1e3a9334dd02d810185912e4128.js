5f4b6ef1b4c6655ae4a1f3288459b0b08f526672
shongie <xongirivisi@gmail.com>
2022-11-24 09:00:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ build/precache-manifest.627cb1e3a9334dd02d810185912e4128.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb46102c339c302b25169307d8998c3",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "6d2f02f09cc22628e480",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0e5377bc5d9b9da4df39",
+    "url": "/react-frontend-dev-portfolio/static/css/main.8a5b2c6b.chunk.css"
+  },
+  {
+    "revision": "6d2f02f09cc22628e480",
+    "url": "/react-frontend-dev-portfolio/static/js/2.6f036064.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/react-frontend-dev-portfolio/static/js/2.6f036064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5377bc5d9b9da4df39",
+    "url": "/react-frontend-dev-portfolio/static/js/main.f22e3aec.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  },
+  {
+    "revision": "1ae29eccf51a5b38509f035f6f40ef30",
+    "url": "/react-frontend-dev-portfolio/static/media/xongi-pic.1ae29ecc.jpg"
+  }
+]);
